5799b26f7978f9a62fae8c50330fc4b8ebe49b83
fxt0706 <fxt0706@gmail.com>
2017-09-19 15:44:20 +0800

    fix the bug

--- youkuvideolist.py
+++ youkuvideolist.py
@@ -7,6 +7,7 @@ file_times = open("youku_video_times.txt","wb")
 file_video = open("youku_video_list.txt","wb")
 xls_data = xlwt.Workbook()
 xls_table=xls_data.add_sheet('videos',cell_overwrite_ok=True)
+xls_line = 1
 
 
 def run():
@@ -16,7 +17,7 @@ def run():
     url = 'https://api.youku.com/videos/by_me.json'
 
     # replace your private parameter below(client_id / access_token / video_page)
-    payloads = {'client_id':'999*******d**0b','access_token':'62****01e363*****dc905c9ff','count':50,'page':1}
+    payloads = {'client_id':'99989ad8221add0b','access_token':'6253aae7227f8701e36367bdc905c9ff','count':50,'page':1}
     video_page = 2
 
     # get the JSON data and set to list
@@ -45,26 +46,27 @@ def run():
     print("Get the video list succeed! \nCheck the two txt files for details")
 
 
-def export_video_list(video_list,youku_text,num,xls_line):
+def export_video_list(video_list,youku_text,num):
     #this function list the video to video_list and export details to .xls
     for i in range(num):
         video_list.append(youku_text['videos'][i]['title'])
+        global xls_line
         xls_table.write(xls_line, 0, youku_text['videos'][i]['id'])
         xls_table.write(xls_line, 1, youku_text['videos'][i]['title'])
         xls_table.write(xls_line, 2, youku_text['videos'][i]['view_count'])
         xls_table.write(xls_line, 3, youku_text['videos'][i]['link'])
+        xls_table.write(xls_line, 4, youku_text['videos'][i]['published'])
         xls_line += 1
 
 
 def trans_json(video_page,video_list,url,payloads):
     #this fuction get the all json files and deal with the data
-    xls_line = 1
     for i in range(1,video_page+1):
         youku_json_get = requests.get(url, params=payloads).json()
         youku_json = json.dumps(youku_json_get)
         youku_text = json.loads(youku_json)
         video_num = len(youku_text['videos']) - 1
-        export_video_list(video_list, youku_text, video_num,xls_line)
+        export_video_list(video_list, youku_text, video_num)
         payloads['page'] = i+1
 
 
@@ -73,6 +75,7 @@ def init_xls():
     xls_table.write(0,1,"title")
     xls_table.write(0,2,"view_count")
     xls_table.write(0,3,"link")
+    xls_table.write(0,4,'published')
 
 
 
